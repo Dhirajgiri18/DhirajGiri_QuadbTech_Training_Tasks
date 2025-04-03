48d5829b05874456dad494b10a15ad3eb64e24f7
Dhiraj Giri <dhirajgiri91124@gmail.com>
2025-04-03 16:59:52 +0000

    Week3 conflicts resolved

--- Week3_Ethereum/Day2/contracts/GasFeeTracker.sol
+++ Week3_Ethereum/Day2/contracts/GasFeeTracker.sol
@@ -8,28 +8,53 @@ contract GasFeeTracker {
         uint256 timestamp;
     }
 
-    TransactionDetails[] private transactions;
+    TransactionDetails[] private storageTransactions;
+    TransactionDetails[] private memoryTransactions;
 
-    event TransactionRecorded(address indexed sender, uint256 gasUsed, uint256 timestamp);
+    event StorageTransactionRecorded(address indexed sender, uint256 gasUsed, uint256 timestamp);
+    event MemoryTransactionRecorded(address indexed sender, uint256 gasUsed, uint256 timestamp);
 
-    function recordTransaction() public returns (uint256) {
+    function recordStorageTransaction() public returns (uint256) {
         uint256 gasStart = gasleft();
         
-        transactions.push(TransactionDetails({
+        storageTransactions.push(TransactionDetails({
             sender: msg.sender,
             gasUsed: gasStart,
             timestamp: block.timestamp
         }));
 
         uint256 gasUsed = gasStart - gasleft();
-        transactions[transactions.length - 1].gasUsed = gasUsed;
+        storageTransactions[storageTransactions.length - 1].gasUsed = gasUsed;
 
-        emit TransactionRecorded(msg.sender, gasUsed, block.timestamp);
+        emit StorageTransactionRecorded(msg.sender, gasUsed, block.timestamp);
         
-        return gasUsed; // Returning gas used for comparison
+        return gasUsed;
     }
 
-    function getAllTransactions() public view returns (TransactionDetails[] memory) {
-        return transactions;
+    function recordMemoryTransaction() public returns (uint256) {
+        uint256 gasStart = gasleft();
+
+        TransactionDetails memory tempTransaction = TransactionDetails({
+            sender: msg.sender,
+            gasUsed: 0, // Temporary, updated after calculation
+            timestamp: block.timestamp
+        });
+
+        uint256 gasUsed = gasStart - gasleft();
+        tempTransaction.gasUsed = gasUsed;
+
+        memoryTransactions.push(tempTransaction);
+
+        emit MemoryTransactionRecorded(msg.sender, gasUsed, block.timestamp);
+
+        return gasUsed;
+    }
+
+    function getStorageTransactions() public view returns (TransactionDetails[] memory) {
+        return storageTransactions;
+    }
+
+    function getMemoryTransactions() public view returns (TransactionDetails[] memory) {
+        return memoryTransactions;
     }
 }
